f3a497cbc55ec77ae6053346f3d4deb4f4d4f234
Hannes Reinecke <hare@suse.de>
2009-10-05 09:08:36 +0200

    Allow empty username for CHAP in discovery
    For discovery we also might need to set empty usernames; not
    all iSCSI target implementations allow to set a username for
    CHAP (also called 'one-way CHAP' there).
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- usr/discovery.c
+++ usr/discovery.c
@@ -615,8 +615,7 @@ setup_authentication(iscsi_session_t *session,
 		session->bidirectional_auth = 1;
 
 		/* sanity check the config */
-		if ((config->auth.username[0] == '\0')
-		    || (config->auth.password_length == 0)) {
+		if (config->auth.password_length == 0) {
 			log_error(
 			       "discovery process to %s:%d has incoming "
 			       "authentication credentials but has no outgoing "
@@ -635,9 +634,12 @@ setup_authentication(iscsi_session_t *session,
 	}
 
 	/* copy in whatever credentials we have */
-	strncpy(session->username, config->auth.username,
-		sizeof (session->username));
-	session->username[sizeof (session->username) - 1] = '\0';
+	if (config->auth.username && strlen(config->auth.username)) {
+		strncpy(session->username, config->auth.username,
+			sizeof (session->username));
+		session->username[sizeof (session->username) - 1] = '\0';
+	} else
+		session->username[0] = '\0';
 	if ((session->password_length = config->auth.password_length))
 		memcpy(session->password, config->auth.password,
 		       session->password_length);
