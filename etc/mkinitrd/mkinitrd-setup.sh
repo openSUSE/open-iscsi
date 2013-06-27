c9765ebce798ba743e4ead9ea0a77e2d1aef4e41
Lee Duncan <lduncan@suse.com>
2013-06-27 14:00:37 -0700

    Port of bnc#630434 fix for non-root boot-time iSCSI volumes

--- etc/mkinitrd/mkinitrd-setup.sh
+++ etc/mkinitrd/mkinitrd-setup.sh
@@ -23,7 +23,7 @@ function check_iscsi()
 {
     local devname="$1" retval=1
     local sysfs_path=$(/sbin/udevadm info -q path -n "${devname}" 2>/dev/null)
-    local ip target startup
+    local ip
 
     # do we have a valid device?
     [[ -z "${sysfs_path}" ]] && sysfs_path="/block${devname##/dev}"
@@ -40,48 +40,59 @@ function check_iscsi()
     ip="$(echo ${sysfs_path}/connection*)"
     verify_path "${devname}" "${ip}" || return 1	# no, return false
 
-    ip="${ip}/iscsi_connection/${ip##*/}/persistent_"
-    [[ ! -r "${ip}address" || ! -r "${ip}port" ]] &&
-	error 2 "iSCSI device ${devname} connected to iSCSI target without any persistent_{address,port}!"
-    ip="$(cat "${ip}address"):$(cat "${ip}port")"
+    # check for node startup value of "onboot", else print a warning
+    check_for_node_onboot $ip
 
+    # attached ot an iSCSI device
+    return 0						# success
+}
+
+
+check_for_node_onboot()
+{
+    local ip="$1" startup target
+
+    # get persistent address and port, for iscsiadm command
+    ip="${ip}/iscsi_connection/${ip##*/}/persistent_"
+    if [[ ! -r "${ip}address" || ! -r "${ip}port" ]] ; then
+	echo >&2 "Warning: iSCSI device ${devname} connected to iSCSI target without any persistent_{address,port}!"
+	return
+    fi
+    ip="$(< "${ip}address"):$(< "${ip}port")"
+
+    # is there a session directory?
     target=$(echo ${sysfs_path}/iscsi_session/*)
-    verify_path "${devname}" "${target}" || return 1	# no, return false
+    verify_path "${devname}" "${target}" || return	# no session: return
 
     target="${target}/targetname"
-    [[ ! -r "${target}" ]] &&
-	error 2 "iSCSI device ${devname} connected to iSCSI target without any targetname!"
-    target="$(cat ${target})"
+    if [[ ! -r "${target}" ]] ; then
+	echo >&2 "Warning: iSCSI device ${devname} connected to iSCSI target without any targetname!"
+	return
+    fi
+    target="$(< ${target})"
 
     # figure out whether it has been correctly configured
-    [[ ! -x "${_iadm_}" ]] &&
-	error 2 "iSCSI device ${devname} connected to iSCSI target, but no ${_iadm_} command available!"
+    if [[ ! -x "${_iadm_}" ]] ; then
+	echo >&2 "Warning: iSCSI device ${devname} connected to iSCSI target, but no ${_iadm_} command available!"
+	return
+    fi
 
     startup="$(
 	${_iadm_} -m node -p "${ip}" -T "${target}" 2>/dev/null | grep 'node.conn\[0\].startup'
     )"
+
     startup="${startup##* }"
     startup="${startup%% *}"
     if [[ "${startup}" != "onboot" ]] ; then
-	[[ -z "${startup}" ]] && return 1	# Oops, no parameter - not an iSCSI, return false
+	[[ -z "${startup}" ]] && return		# No parameter - either iBFT or not iSCSI: return
 
-	## Either:
-	#echo >&2 "WARNING: iSCSI device ${devname} is using 'node.conn[0].startup = ${startup}',"
-	#echo >&2 "WARNING: setting it to 'onboot' instead."
-	#${_iadm_} -m node -p "${ip}" -T "${target}" -o update -n 'node.conn[0].startup' -v onboot
-
-	## or:
-	echo >&2 "WARNING: iSCSI device ${devname} is using 'node.conn[0].startup = ${startup}'!"
-	echo >&2 "WARNING: System not bootable with this setting, need to be set to 'onboot' instead, using:"
-	echo >&2 "
-	${_iadm_} -m node -p '${ip}' -T '${target}' -o update -n 'node.conn[0].startup' -v onboot"
+	## Note: could set "onboot" ourselves here, but that seems heavy-handed
+	echo >&2 "Warning: iSCSI device ${devname} is using 'node.conn[0].startup = ${startup}'"
+	echo >&2 "         System may not be bootable with this setting,"
+	echo >&2 "         need to be set to 'onboot' instead, using:"
+	echo >&2 "   ${_iadm_} -m node -p '${ip}' -T '${target}' -o update -n 'node.conn[0].startup' -v onboot"
     fi
-
-    # attached to an iSCSI device
-    return 0
 }
-
-
 
 _iadm_="/sbin/iscsiadm"
 
@@ -100,18 +111,20 @@ done
 # (1) the iSCSI gets included in "initrd", and
 # (2) the iSCSI sessions have been configured with
 #     "node.conn[0].startup = onboot".
-for bd in $(awk '/^[[:space:]]*(\/dev\/|(LABEL|UUID)=)/ { print $1 }' /etc/fstab)
-do
-    bd="${bd/LABEL=//dev/disk/by-label/}"
-    bd="${bd/UUID=//dev/disk/by-uuid/}"
-    update_blockdev $bd
-    check_iscsi $bd && root_iscsi=1
-done
+if [[ -z "${root_iscsi}" ]] ; then
+    for bd in $(awk '/^[[:space:]]*(\/dev\/|(LABEL|UUID)=)/ { print $1 }' /etc/fstab) ; do
+	bd="${bd/LABEL=//dev/disk/by-label/}"
+	bd="${bd/UUID=//dev/disk/by-uuid/}"
+	update_blockdev $bd
+	check_iscsi $bd && root_iscsi=1
+    done
+fi
+
 
 # Include the iSCSI stack, when at least one active iSCSI session has
 # been configured with "node.conn[0].startup = onboot", even if it was
 # not used for a system device or mounted partition.
-if [[ -x "${_iadm_}" ]] ; then
+if [[ -x "${_iadm_}" && -z "${root_iscsi}" ]] ; then
     for node in $(${_iadm_} -m node 2>/dev/null | sed -e 's/ /,/g') ; do
 	[[ "$(
 		${_iadm_} -m node -T "${node##*,}" -p "${node%%,*}" 2>/dev/null |
@@ -124,13 +137,11 @@ unset _iadm_
 
 save_var root_iscsi
 
-if [ "${root_iscsi}" ]; then
+if [[ "${root_iscsi}" ]] ; then
     # copy the iscsi configuration
     cp -rp /etc/iscsi etc/
 
-    if [ -z "$interface" ] ; then
-	interface="default"				# needed?
-    fi
+    [[ -z "$interface" ]] && interface="default"
 
     # In case target port was not defined via command line, assign default port
     save_var TargetPort 3260
